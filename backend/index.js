7e0e8042a8fdaa4ac328b7db7a9e678e4e0b82e4
Ahmad-Mridul <mridulahmed262@gmail.com>
2025-12-14 17:31:03 +0600

    Fixed .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -13,7 +14,7 @@ app.use(cors());
 
 
 
-const uri = "mongodb+srv://birm2476:birm2476@cluster0.cqkzp8h.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqkzp8h.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
